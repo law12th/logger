897ee4434b0d459fb3a3f141689c4d24769905a8
law12th <lawrenctepula@gmail.com>
2023-11-13 23:38:56 +0200

    feat: add application constant variables

---
+++ src/utils/constants.ts
@@ -0,0 +1,28 @@
+export const DATE_FORMAT = 'YYYY-MM-DD'
+
+export const TIME_FORMAT = 'HH:mm:ss:ms'
+
+export enum Colours {
+  BLACK = 'black',
+  BLUE = 'blue',
+  GOLD = 'gold',
+  GRAY = 'gray',
+  GREEN = 'green',
+  MAGENTA = 'magenta',
+  ORANGE = 'orange',
+  PINK = 'pink',
+  PURPLE = 'purple',
+  RED = 'red',
+  WHITE = 'white',
+  YELLOW = 'yellow'
+}
+
+export enum LoggingLevels {
+  ERROR,
+  WARN,
+  INFO,
+  HTTP,
+  VERBOSE,
+  DEBUG,
+  SILLY
+}
